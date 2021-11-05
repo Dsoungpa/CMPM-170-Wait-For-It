00377e16c3a9e9fa6ccad4eed69bb843ee2c10aa
yli530 <yli530@ucsc.edu>
2021-11-04 22:29:30 -0700

    implemented gameloop ticking, dynamic resource update on html

--- main.js
+++ main.js
@@ -29,31 +29,34 @@
 //Base resources object
 //Driven is a "fake" number used to represent driving off zombies (1 leaves when it hits 1)
 //Code is simpler with it included here
-let resources = {food: 20, materials: 30, medicine: 3, driven: 0};
+let resources = { food: 20, materials: 30, medicine: 3, driven: 0 };
 //Occupations
 /** @type {Occupation} */
-let  foodScav = {
+let foodScav = {
     Name: "Food Scavaging",
-    Products: [{resource: "food", quant: 0.25, chance: 1}, {resource: "medicine", quant: 1, chance: 0.05}],
+    Products: [{ resource: "food", quant: 0.25, chance: 1 }, { resource: "medicine", quant: 1, chance: 0.05 }],
     Enabled: true,
     Assigned: 0
 }
 
 /** @type {Occupation} */
-let  matScav = {
+let matScav = {
     Name: "Material Scavaging",
-    Products: [{resource: "food", quant: 0.25, chance: 1}, {resource: "medicine", quant: 1, chance: 0.05}],
+    Products: [{ resource: "food", quant: 0.25, chance: 1 }, { resource: "medicine", quant: 1, chance: 0.05 }],
     Enabled: true,
     Assigned: 0
 }
 
 /** @type {Occupation} */
-let  protecting = {
+let protecting = {
     Name: "Scaring off zombies",
-    Products: [{resource: "driven", quant: 0.2, chance: 1}],
+    Products: [{ resource: "driven", quant: 0.2, chance: 1 }],
     Enabled: true,
     Assigned: 0
 }
+
+//How long it takes for an update tick to happen(in ms)
+let tickRate = 1000;
 
 let occupations = [foodScav, matScav, protecting]
 let totPop = 3;
@@ -61,53 +64,50 @@ let availPop = 3;
 let closeZom = 0;
 let totZom = 100000;
 let zomTime = 0;
-let rollTimer = 0
+let rollTimer = 0;
+let foodRate = 0;
 
-while(false){
+function gameLoop() {
     rollTimer += 0.01;
+    foodRate = totPop * -0.02;
     //Add the resources from the last tick
     occupations.forEach((val) => {
-        if(val.Enabled && val.Assigned > 0)
+        if (val.Enabled && val.Assigned > 0)
             val.Products.forEach((gainObj) => {
-                if(1== gainObj.chance)
+                if (1 == gainObj.chance)
                     resources[gainObj.resource] += gainObj.quant * val.Assigned * 0.01
                 //Handles repeated rolling for chance-based thing
                 //Each 10th of the assigned units gets 1 roll
                 //Roll only once every second (for now 1 second = 100 ticks)
-                else if(rollTimer >= 1){
+                else if (rollTimer >= 1) {
                     //Make sure that if we have less than 10 assigned we do that number of rolls
                     let numRolls = Math.min(val.Assigned, 10);
                     //Similarly, make sure we have a whole number (min 1) gains per success
-                    let successfulUnits = Math.max(1, Math.floor(val.Assigned/10))
-                    for(let i = 0; i < numRolls; i++){
-                        if(Math.random() < gainObj.chance)
+                    let successfulUnits = Math.max(1, Math.floor(val.Assigned / 10))
+                    for (let i = 0; i < numRolls; i++) {
+                        if (Math.random() < gainObj.chance)
                             resources[gainObj.resource] += gainObj.quant * successfulUnits
                     }
                 }
             })
     })
-    if(rollTimer >= 1)
+    if (rollTimer >= 1)
         rollTimer = 0;
-    resources.food -= totPop * 0.02;
+    resources.food += foodRate;
     //handles zombie arrivals
     zomTime += 0.01;
-    if(zomTime >= 30/Math.max(totPop, 1)){
+    if (zomTime >= 30 / Math.max(totPop, 1)) {
         zomTime = 0;
         closeZom += 1;
     }
     //And being driven off
     //slightly more complex than it needs to be to deal with having many kicking zombies out
-    if(resources.driven >= 1){
+    if (resources.driven >= 1) {
         closeZom -= Math.min(Math.floor(resources.driven), closeZom)
         resources.driven = 0;
     }
-    //just output all resources/info for now
-    for (let oct in occupations)
-        console.log(oct)
-    for (let res in resources)
-        console.log(res)
-    console.log("curr pop " + totPop + " avail pop " + availPop)
-    console.log("close zombies/total zombies " + closeZom + "/" + totZom)
+
+    updateNums();
 }
 
 //Function for manually assigning population to a task
@@ -117,7 +117,7 @@ while(false){
  * @param numAssign: {Object}
  * @param occupationAdded : {Number}
  */
-function assignPop(numAssign, occupationAdded){
+function assignPop(numAssign, occupationAdded) {
     //Get the number we're assigning
     let baseAssign = parseInt(numAssign.getElementsByTagName("input")[0].value);
     //If there's not enough population to assign, assign as many as possible
@@ -130,11 +130,12 @@ function assignPop(numAssign, occupationAdded){
     occupations[occupationAdded].Assigned += actAssign;
     availPop -= actAssign;
     console.log(occupations[occupationAdded].Assigned)
+    updateNums();
 }
 
 //Function for curing population
-function cureZombie(){
-    if(resources.medicine >= 1 && closeZom >= 1){
+function cureZombie() {
+    if (resources.medicine >= 1 && closeZom >= 1) {
         resources.medicine -= 1;
         availPop += 1;
         totPop += 1;
@@ -143,7 +144,45 @@ function cureZombie(){
     }
 }
 
-while(false){
-    for (let occ in occupations)
-        console.log(occ)
+function updateHuman() {
+    let human = document.getElementById("human");
+    human.innerHTML = availPop + " / " + totPop;
 }
+
+function updateZombie() {
+    let zombie = document.getElementById("zombie");
+    zombie.innerHTML = totZom.toString();
+}
+
+function updateMed() {
+    let med = document.getElementById("medicine");
+    med.innerHTML = resources.medicine.toString();
+}
+
+function updateFood() {
+    let food = document.getElementById("food");
+    food.innerHTML = (Math.round(resources.food * 100) / 100).toString();
+    let fRate = document.getElementById("foodRate");
+    fRate.innerHTML = ((foodRate < 0) ? "-" : "") + (Math.round(totPop * 2) / 100) + "/s"
+}
+
+function updateMaterials() {
+    let mat = document.getElementById("material");
+    mat.innerHTML = resources.materials.toString();
+}
+
+
+function updateNums() {
+    updateHuman();
+    updateZombie();
+    updateMed();
+    updateFood();
+    updateMaterials()
+}
+
+gameLoop();
+var timer = function () {
+    gameLoop();
+    setTimeout(timer, tickRate);
+}
+setTimeout(timer, tickRate);
