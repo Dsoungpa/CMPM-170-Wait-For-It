635719c912d3b9edb1d733885a356716cbd0a2d3
Darcy Phipps <diphipps@ucsc.edu>
2021-11-05 12:58:12 -0700

    Fixed resource display

--- main.js
+++ main.js
@@ -30,6 +30,8 @@
 //Driven is a "fake" number used to represent driving off zombies (1 leaves when it hits 1)
 //Code is simpler with it included here
 let resources = { food: 20, materials: 30, medicine: 3, driven: 0 };
+//Also store the change in each resource
+let resourceChanges = { food: 0, materials: 0, medicine: 0, driven: 0 };
 //Occupations
 /** @type {Occupation} */
 let foodScav = {
@@ -64,38 +66,48 @@ let availPop = 3;
 let closeZom = 0;
 let totZom = 100000;
 let zomTime = 0;
-let rollTimer = 0;
 let foodRate = 0;
 
 function gameLoop() {
-    rollTimer += 0.01;
     foodRate = totPop * -0.02;
+    //Reset prior resource changes
+    for (const resourceC in resourceChanges)
+        resourceChanges[resourceC] = 0
     //Add the resources from the last tick
     occupations.forEach((val) => {
         if (val.Enabled && val.Assigned > 0)
             val.Products.forEach((gainObj) => {
-                if (1 == gainObj.chance)
-                    resources[gainObj.resource] += gainObj.quant * val.Assigned * 0.01
+                if (1 == gainObj.chance){
+                    resources[gainObj.resource] += gainObj.quant * val.Assigned
+                    resourceChanges[gainObj.resource] += gainObj.quant * val.Assigned
+                }
                 //Handles repeated rolling for chance-based thing
                 //Each 10th of the assigned units gets 1 roll
                 //Roll only once every second (for now 1 second = 100 ticks)
-                else if (rollTimer >= 1) {
+                else {
                     //Make sure that if we have less than 10 assigned we do that number of rolls
                     let numRolls = Math.min(val.Assigned, 10);
                     //Similarly, make sure we have a whole number (min 1) gains per success
                     let successfulUnits = Math.max(1, Math.floor(val.Assigned / 10))
                     for (let i = 0; i < numRolls; i++) {
-                        if (Math.random() < gainObj.chance)
+                        if (Math.random() < gainObj.chance){
                             resources[gainObj.resource] += gainObj.quant * successfulUnits
+                            resourceChanges[gainObj.resource] += gainObj.quant * successfulUnits
+                        }
                     }
                 }
             })
     })
-    if (rollTimer >= 1)
-        rollTimer = 0;
     resources.food += foodRate;
+    resourceChanges.food += foodRate
+    for (const resource in resources){
+        if(resources[resource] < 0)
+            resources[resource] = 0
+    }
+    for (const resourceC in resourceChanges)
+        resourceChanges[resourceC] = resourceChanges[resourceC].toFixed(2)
     //handles zombie arrivals
-    zomTime += 0.01;
+    zomTime += tickRate;
     if (zomTime >= 30 / Math.max(totPop, 1)) {
         zomTime = 0;
         closeZom += 1;
@@ -122,9 +134,7 @@ function assignPop(numAssign, occupationAdded) {
     let baseAssign = parseInt(numAssign.getElementsByTagName("input")[0].value);
     //If there's not enough population to assign, assign as many as possible
     //Obviously this check won't matter if we're taking away (numAssign negative)
-    console.log(baseAssign)
     let actAssign = Math.min(baseAssign, availPop)
-    console.log(actAssign)
     //If we are negative though, we can't take away more people than are assigned
     actAssign = Math.max(actAssign, -occupations[occupationAdded].Assigned)
     occupations[occupationAdded].Assigned += actAssign;
@@ -163,7 +173,7 @@ function updateFood() {
     let food = document.getElementById("food");
     food.innerHTML = (Math.round(resources.food * 100) / 100).toString();
     let fRate = document.getElementById("foodRate");
-    fRate.innerHTML = ((foodRate < 0) ? "-" : "") + (Math.round(totPop * 2) / 100) + "/s"
+    fRate.innerHTML = resourceChanges.food + "/s"
 }
 
 function updateMaterials() {
